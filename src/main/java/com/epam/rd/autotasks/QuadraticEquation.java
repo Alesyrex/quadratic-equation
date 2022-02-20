b4101e2ab3d5e2a391f4107d4ed3f7e1fb8685b1
Alesyrex <Alesyrex@gmail.com>
2022-02-20 22:20:05 +0300

    add const

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -6,21 +6,25 @@ import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 
 public class QuadraticEquation {
+    public static final double DEGREE = 2;
+    public static final double MULTIPLIEROFD = 4;
+    public static final double MULTIPLIEROFX = 2;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
 
-        double discriminant = pow(b,2) - 4 * a * c;
+        double discriminant = pow(b,DEGREE) - MULTIPLIEROFD * a * c;
         if (discriminant < 0) {
             System.out.println("no roots");
         } else if (discriminant == 0) {
-            double x = -(b / (2 * a));
+            double x = -(b / (MULTIPLIEROFX * a));
             System.out.println(x);
         } else {
-            double x1 = ((-b) - sqrt(discriminant)) / (2 * a);
-            double x2 = ((-b) + sqrt(discriminant)) / (2 * a);
+            double x1 = ((-b) - sqrt(discriminant)) / (MULTIPLIEROFX * a);
+            double x2 = ((-b) + sqrt(discriminant)) / (MULTIPLIEROFX * a);
             System.out.println(x1 + " " + x2);
         }
     }
