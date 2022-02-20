55f6659fe553c4f0e7d3ff251b2e07f5f60038f3
Alesyrex <Alesyrex@gmail.com>
2022-02-20 22:11:02 +0300

    equation through discriminant

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -2,6 +2,7 @@ package com.epam.rd.autotasks;
 
 import java.util.Scanner;
 
+import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 
 public class QuadraticEquation {
@@ -11,8 +12,17 @@ public class QuadraticEquation {
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
 
-
-
+        double discriminant = pow(b,2) - 4 * a * c;
+        if (discriminant < 0) {
+            System.out.println("no roots");
+        } else if (discriminant == 0) {
+            double x = -(b / (2 * a));
+            System.out.println(x);
+        } else {
+            double x1 = ((-b) - sqrt(discriminant)) / (2 * a);
+            double x2 = ((-b) + sqrt(discriminant)) / (2 * a);
+            System.out.println(x1 + " " + x2);
+        }
     }
 
 }
